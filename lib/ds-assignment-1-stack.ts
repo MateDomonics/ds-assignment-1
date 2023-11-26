fa786c4f44c1aebbd7b397c7560df88c03969eea
Máté Domonics <domonicsmate@gmail.com>
2023-11-26 18:00:55 +0000

    Step 7: Signup, Signin, Signout added, but no validation yet.

--- lib/ds-assignment-1-stack.ts
+++ lib/ds-assignment-1-stack.ts
@@ -7,8 +7,14 @@ import { generateBatch } from '../shared/util';
 import { reviews } from '../seed/reviewsSeed';
 import { Construct } from 'constructs';
 import * as apig from "aws-cdk-lib/aws-apigateway";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
+import * as node from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class dsAssignment1Stack extends cdk.Stack {
+  private auth: apig.IResource;
+  private userPoolId: string;
+  private userPoolClientId: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -125,7 +131,88 @@ export class dsAssignment1Stack extends cdk.Stack {
     const movieReviewByAuthorEndpoint = movieReviewsEndpoint.addResource("{reviewerName}");
     movieReviewByAuthorEndpoint.addMethod(
       "GET",
-      new apig.LambdaIntegration(getMovieReviewByAuthor, {proxy: true})
+      new apig.LambdaIntegration(getMovieReviewByAuthor, { proxy: true })
     )
+
+    const userPool = new UserPool(this, "Assignment1UserPool", {
+      signInAliases: { username: true, email: true },
+      selfSignUpEnabled: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.userPoolId = userPool.userPoolId;
+
+    const appClient = userPool.addClient("Assignment1AppClient", {
+      authFlows: { userPassword: true },
+    });
+
+    this.userPoolClientId = appClient.userPoolClientId;
+
+    const authApi = new apig.RestApi(this, "Assignment1AuthServiceApi", {
+      description: "Authentication Service RestApi",
+      endpointTypes: [apig.EndpointType.REGIONAL],
+      defaultCorsPreflightOptions: {
+        allowOrigins: apig.Cors.ALL_ORIGINS,
+      },
+    });
+
+    this.auth = authApi.root.addResource("auth");
+
+    this.addAuthRoute(
+      "signup",
+      "POST",
+      "SignupFn",
+      'signup.ts'
+    );
+
+    this.addAuthRoute(
+      "confirm_signup",
+      "POST",
+      "ConfirmFn",
+      "confirm-signup.ts"
+    );
+
+    this.addAuthRoute('signout',
+      'GET',
+      'SignoutFn',
+      'signout.ts'
+    );
+
+    this.addAuthRoute('signin',
+      'POST',
+      'SigninFn',
+      'signin.ts'
+    );
+
   }
+
+  private addAuthRoute(
+    resourceName: string,
+    method: string,
+    fnName: string,
+    fnEntry: string,
+    allowCognitoAccess?: boolean
+  ): void {
+    const commonFnProps = {
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION
+      },
+    };
+
+    const resource = this.auth.addResource(resourceName);
+
+    const fn = new node.NodejsFunction(this, fnName, {
+      ...commonFnProps,
+      entry: `${__dirname}/../lambda/auth/${fnEntry}`,
+    });
+
+    resource.addMethod(method, new apig.LambdaIntegration(fn));
+  }  // end private method
 }
--- shared/types.d.ts
+++ shared/types.d.ts
@@ -5,3 +5,19 @@ export type Review = {
     content: string;
     rating: number;
 }
+
+export type SignUpBody = {
+    username: string;
+    password: string;
+    email: string
+}
+
+export type ConfirmSignUpBody = {
+    username: string;
+    code: string;
+}
+
+export type SignInBody = {
+    username: string;
+    password: string;
+}
